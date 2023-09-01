7766651068781b057d8ae6d47727b238f49c0564
Avram Eisner <avrame@gmail.com>
2023-08-31 17:07:03 -0700

    some updates

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,10 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			serif: "Charter, 'Bitstream Charter', 'Sitka Text', Cambria, serif",
+			sans: "Avenir, Montserrat, Corbel, 'URW Gothic', source-sans-pro, sans-serif",
+		},
 		extend: {},
 	},
 	plugins: [],
