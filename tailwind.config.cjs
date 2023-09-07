98b53d8dba2d93e1fb22fc0a995961b52872c0f9
Avram Eisner <avrame@gmail.com>
2023-09-07 16:28:47 -0700

    added dark mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		fontFamily: {
-			serif: "Charter, 'Bitstream Charter', 'Sitka Text', Cambria, serif",
-			sans: "Avenir, Montserrat, Corbel, 'URW Gothic', source-sans-pro, sans-serif",
-		},
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    fontFamily: {
+      serif: "Charter, 'Bitstream Charter', 'Sitka Text', Cambria, serif",
+      sans: "Avenir, Montserrat, Corbel, 'URW Gothic', source-sans-pro, sans-serif",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
