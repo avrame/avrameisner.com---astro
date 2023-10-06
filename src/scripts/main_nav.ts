1a1c2cde6758914a18edd96e5cbb953aa44b26ce
Avram Eisner <avrame@gmail.com>
2023-10-05 17:14:49 -0700

    - added passive: true to some events - changed art/code link title to white for better contrast

--- src/scripts/main_nav.ts
+++ src/scripts/main_nav.ts
@@ -4,8 +4,8 @@ const close_nav_btn = document.getElementById("close_nav_btn");
 
 open_nav_btn?.addEventListener("click", () => {
   main_nav?.classList.remove("-translate-x-full");
-});
+}, { passive: true });
 
 close_nav_btn?.addEventListener("click", () => {
   main_nav?.classList.add("-translate-x-full");
-});
+}, { passive: true });
